fe7605cbefd981f52d285dc8bbe7ada6722e003c
MartianKingg <bibhu.pupun123@gmail.com>
2024-05-27 17:45:47 +0530

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('users/{p_para}', function($para){
+Route::get('users/{p_para}', function ($para) {
     return $para;
 });
 
 // route parameter and regular expression
-Route::get('/product/{p_name}',function($name){
+Route::get('/product/{p_name}', function ($name) {
     return $name;
-})->where('p_name','[A-Za-z]+');
+})->where('p_name', '[A-Za-z]+');
 
+Route::get('/no-access', function () {
+    echo "You are not access to this page";
+    die;
+});
+
+Route::get('/login', function () {
+    session()->put('user_id', 1);
+    return redirect('customer');
+});
+
+Route::get('/logout', function () {
+    session()->forget('user_id', 1);
+    return redirect()->back();
+});
 
 // route for displaying data from db using model
-Route::get('/customer',function(){
+Route::get('/customer', function () {
     $customer = Customers::all();
 
     echo '<pre>';
@@ -38,11 +52,17 @@ Route::get('/customer',function(){
     print_r($customer->toArray());
 });
 
+// group middleware
+Route::middleware(['guard','web'])->group(function () {
+    //protected
 
-// Route for customer 
-Route::get('/customer',[CustomerController::class, 'index']);
-Route::get('/customer/add_customer',[CustomerController::class, 'add_customer'])->name('customer.add_customer');
-Route::post('/customer/register',[CustomerController::class, 'register']);
-Route::get('/customer/delete/{id}',[CustomerController::class, 'delete'])->name('customer.delete');
-Route::get('/customer/edit/{id}',[CustomerController::class, 'edit'])->name('customer.edit');
-Route::post('/customer/update/{id}',[CustomerController::class, 'update'])->name('customer.update');
+    // Route for customer 
+    // Route::get('/customer', [CustomerController::class, 'index'])->middleware('guard');  //for route middleware
+
+    Route::get('/customer', [CustomerController::class, 'index']);
+    Route::get('/customer/add_customer', [CustomerController::class, 'add_customer'])->name('customer.add_customer');
+    Route::post('/customer/register', [CustomerController::class, 'register']);
+    Route::get('/customer/delete/{id}', [CustomerController::class, 'delete'])->name('customer.delete');
+    Route::get('/customer/edit/{id}', [CustomerController::class, 'edit'])->name('customer.edit');
+    Route::post('/customer/update/{id}', [CustomerController::class, 'update'])->name('customer.update');
+});
